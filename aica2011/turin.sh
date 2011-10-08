66f15cf7ffea915d16a3135d63f642cbc286c8cc
Simone Basso <bassosimone@gmail.com>
2011-10-08 20:52:07 +0200

    turin.sh: add upload plus minor fixes

--- aica2011/turin.sh
+++ aica2011/turin.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Public domain.  Simone Basso <bassosimone@gmail.com>
 
-bunzip2 -c turin.json.bz2 > turin.json
+test -f turin.json || bunzip2 -c turin.json.bz2 > turin.json
 
-./hist_stats.py turin.json > table.tex
+test -f table.tex || ./hist_stats.py turin.json > table.tex
 
-./hist_plot.py -C \
+test -f turin-latency.pdf || ./hist_plot.py -C \
   -D '/AS30722 Vodafone N.V./speedtest/latency' \
   -D '/AS1267 Infostrada S.p.A./speedtest/latency' \
   -D '/AS12874 Fastweb SpA/speedtest/latency' \
@@ -16,7 +16,7 @@ bunzip2 -c turin.json.bz2 > turin.json
   -o turin-latency.pdf \
   turin.json
 
-./hist_plot.py -C \
+test -f turin-dload.pdf || ./hist_plot.py -C \
   -D '/AS30722 Vodafone N.V./speedtest/download_speed' \
   -D '/AS1267 Infostrada S.p.A./speedtest/download_speed' \
   -D '/AS12874 Fastweb SpA/speedtest/download_speed' \
@@ -27,13 +27,24 @@ bunzip2 -c turin.json.bz2 > turin.json
   -o turin-dload.pdf \
   turin.json
 
-./hist_plot.py -C \
+test -f turin-bdp.pdf || ./hist_plot.py -C \
   -D '/AS30722 Vodafone N.V./speedtest/download_wnd' \
   -D '/AS1267 Infostrada S.p.A./speedtest/download_wnd' \
   -D '/AS12874 Fastweb SpA/speedtest/download_wnd' \
   -D '/AS3269 Telecom Italia S.p.a./speedtest/download_wnd' \
-  -F KBytes -L 0 -N -n 10000 -S -U 300 \
-  -T 'Cumulative bandwidth-delay product distribution' \
-  -Y Frequency -X 'Bandwidth-delay product [KBytes]' \
+  -N -F KByte -L 0 -n 10000 -S -U 512 \
+  -T 'Cumulative download bandwidth-delay product distribution' \
+  -Y Frequency -X 'Download bandwidth-delay product [KByte]' \
   -o turin-bdp.pdf \
+  turin.json
+
+test -f turin-upload.pdf || ./hist_plot.py -C \
+  -D '/AS30722 Vodafone N.V./speedtest/upload_speed' \
+  -D '/AS1267 Infostrada S.p.A./speedtest/upload_speed' \
+  -D '/AS12874 Fastweb SpA/speedtest/upload_speed' \
+  -D '/AS3269 Telecom Italia S.p.a./speedtest/upload_speed' \
+  -N -F Kbit/s -L 0 -n 10000 -S -U 2000 \
+  -T 'Cumulative upload goodput distribution' \
+  -Y Frequency -X 'Upload goodput [Kbit/s]' \
+  -o turin-upload.pdf \
   turin.json
